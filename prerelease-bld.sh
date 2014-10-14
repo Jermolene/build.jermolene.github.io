ec15e6682a62faaff8150fc33a78435394d11092
Jermolene <jeremy@osmosoft.com>
2014-10-14 15:44:52 +0100

    First commit

---
+++ prerelease-bld.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Build all tiddlywiki.com assets using the version of tiddlywiki specified in package.json
+
+# Use the pre-release edition as the main edition
+
+export TW5_BUILD_MAIN_EDITION=../TiddlyWiki5/editions/prerelease
+
+# Use the pre-release version of tiddlywiki
+
+export TW5_BUILD_TIDDLYWIKI=./tiddlywiki.js
+
+# Set up the build output directory
+
+if [  -z "$TW5_BUILD_OUTPUT" ]; then
+    export TW5_BUILD_OUTPUT=../jermolene.github.io/prerelease
+fi
+
+mkdir -p $TW5_BUILD_OUTPUT
+
+# Invoke the main bld script
+../build.jermolene.github.io/bld.sh
