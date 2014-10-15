0ffbe3877db36847c3aa1a15393bee8a197cd3b5
Jermolene <jeremy@osmosoft.com>
2014-10-15 21:53:35 +0100

    Add dev-bld and quick-bld

---
+++ dev-bld.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Abbreviated build script for building prerelease
+
+# Use the pre-release edition as the main edition
+
+export TW5_BUILD_MAIN_EDITION=../TiddlyWiki5/editions/prerelease
+
+# Use the pre-release version of tiddlywiki
+
+export TW5_BUILD_TIDDLYWIKI=./tiddlywiki.js
+
+# Set up the build output directory
+
+if [  -z "$TW5_BUILD_OUTPUT" ]; then
+    export TW5_BUILD_OUTPUT=../jermolene.github.io/prerelease
+fi
+
+mkdir -p $TW5_BUILD_OUTPUT
+
+# Build editions
+
+node $TW5_BUILD_TIDDLYWIKI \
+	../TiddlyWiki5/editions/dev \
+	--verbose \
+	--output $TW5_BUILD_OUTPUT/dev \
+	--build index favicon \
+	|| exit 1
