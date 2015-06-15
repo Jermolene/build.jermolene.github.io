579eb6156b9e26bcdab527ef9217e216387238ca
Jermolene <jeremy@jermolene.com>
2015-06-15 15:43:02 +0100

    Update plugin library versions

--- make-library-bld.sh
+++ make-library-bld.sh
@@ -5,7 +5,7 @@
 # Default to the current version
 
 if [  -z "$TW5_BUILD_VERSION" ]; then
-    TW5_BUILD_VERSION=v5.1.8
+    TW5_BUILD_VERSION=v5.1.9
 fi
 
 # Use the pre-release edition as the main edition
