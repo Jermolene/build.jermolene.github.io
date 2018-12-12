dbbc1fa0d10c6f38337481d4a1564fa64b61ca70
Jermolene <jeremy@jermolene.com>
2018-12-12 11:40:21 +0000

    Rationalise scripts
    `bld-docs.sh`, `dev-bld.sh` and `make-library-bld.sh` were trimmed versions of `bld.sh` used to optimise development. They aren't needed if we're targetting production builds
    
    `languages-bld.sh` was an attempt at encapsulation but I think it just made things more complicated.
    
    Addreses part of #4

--- bld.sh
+++ bld.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Build all tiddlywiki.com assets using the version of tiddlywiki specified in package.json
+# Build all tiddlywiki.com assets
 
-# Default to the current version
+# Default to the current version number for building the plugin library
 
 if [  -z "$TW5_BUILD_VERSION" ]; then
     TW5_BUILD_VERSION=v5.1.18
@@ -265,7 +265,88 @@ node $TW5_BUILD_TIDDLYWIKI \
 #
 ######################################################
 
-../build.jermolene.github.io/languages-bld.sh
+# Delete any existing static content
+
+rm $TW5_BUILD_OUTPUT/languages/de-AT/static/*
+rm $TW5_BUILD_OUTPUT/languages/de-DE/static/*
+rm $TW5_BUILD_OUTPUT/languages/es-ES/static/*
+rm $TW5_BUILD_OUTPUT/languages/fr-FR/static/*
+rm $TW5_BUILD_OUTPUT/languages/ja-JP/static/*
+rm $TW5_BUILD_OUTPUT/languages/ko-KR/static/*
+rm $TW5_BUILD_OUTPUT/languages/zh-Hans/static/*
+rm $TW5_BUILD_OUTPUT/languages/zh-Hant/static/*
+
+# /languages/de-AT/index.html		Demo wiki with de-AT language
+# /languages/de-AT/empty.html		Empty wiki with de-AT language
+node $TW5_BUILD_TIDDLYWIKI \
+	../TiddlyWiki5/editions/de-AT \
+	--verbose \
+	--output $TW5_BUILD_OUTPUT/languages/de-AT \
+	--build favicon empty static index \
+	|| exit 1
+
+# /languages/de-DE/index.html		Demo wiki with de-DE language
+# /languages/de-DE/empty.html		Empty wiki with de-DE language
+node $TW5_BUILD_TIDDLYWIKI \
+	../TiddlyWiki5/editions/de-DE \
+	--verbose \
+	--output $TW5_BUILD_OUTPUT/languages/de-DE \
+	--build favicon empty static index \
+	|| exit 1
+
+# /languages/es-ES/index.html		Demo wiki with es-ES language
+# /languages/es-ES/empty.html		Empty wiki with es-ES language
+node $TW5_BUILD_TIDDLYWIKI \
+	../TiddlyWiki5/editions/es-ES \
+	--verbose \
+	--output $TW5_BUILD_OUTPUT/languages/es-ES \
+	--build favicon empty static index \
+	|| exit 1
+
+# /languages/fr-FR/index.html		Demo wiki with fr-FR language
+# /languages/fr-FR/empty.html		Empty wiki with fr-FR language
+node $TW5_BUILD_TIDDLYWIKI \
+	../TiddlyWiki5/editions/fr-FR \
+	--verbose \
+	--output $TW5_BUILD_OUTPUT/languages/fr-FR \
+	--build favicon empty static index \
+	|| exit 1
+
+# /languages/ja-JP/index.html		Demo wiki with ja-JP language
+# /languages/ja-JP/empty.html		Empty wiki with ja-JP language
+node $TW5_BUILD_TIDDLYWIKI \
+	../TiddlyWiki5/editions/ja-JP \
+	--verbose \
+	--output $TW5_BUILD_OUTPUT/languages/ja-JP \
+	--build empty index \
+	|| exit 1
+
+# /languages/ko-KR/index.html		Demo wiki with ko-KR language
+# /languages/ko-KR/empty.html		Empty wiki with ko-KR language
+node $TW5_BUILD_TIDDLYWIKI \
+	../TiddlyWiki5/editions/ko-KR \
+	--verbose \
+	--output $TW5_BUILD_OUTPUT/languages/ko-KR \
+	--build favicon empty static index \
+	|| exit 1
+
+# /languages/zh-Hans/index.html		Demo wiki with zh-Hans language
+# /languages/zh-Hans/empty.html		Empty wiki with zh-Hans language
+node $TW5_BUILD_TIDDLYWIKI \
+	../TiddlyWiki5/editions/zh-Hans \
+	--verbose \
+	--output $TW5_BUILD_OUTPUT/languages/zh-Hans \
+	--build empty index \
+	|| exit 1
+
+# /languages/zh-Hant/index.html		Demo wiki with zh-Hant language
+# /languages/zh-Hant/empty.html		Empty wiki with zh-Hant language
+node $TW5_BUILD_TIDDLYWIKI \
+	../TiddlyWiki5/editions/zh-Hant \
+	--verbose \
+	--output $TW5_BUILD_OUTPUT/languages/zh-Hant \
+	--build empty index \
+	|| exit 1
 
 ######################################################
 #
